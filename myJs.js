a7580feb6c69f8f986423e092b7cf899ce130f02
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-10-23 14:36:33 +0700

    No hope

---
+++ myJs.js
@@ -0,0 +1,28 @@
+var celebrationNames = [
+    'Ngô Thị Ngọc Ánh',
+    'Ngô Thị Mai',
+    'Giang Thị Tình',
+    'Giang Thị Hạnh',
+    'Trần Thị Hương Ly',
+    'Đặng Danh Quảng'
+];
+
+
+var show = document.getElementById('show-more');
+show.onclick = function () {
+    this.classList.add('hidden-tag');
+    var listName = document.getElementById('list-name');
+    for (var i = 0; i <celebrationNames.length ; i++) {
+        listName.innerHTML += '<li>'+ celebrationNames[i] + '</li>'
+    }
+}
+
+
+
+
+
+
+
+
+
+
